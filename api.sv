9b1729f681bf7e64027000c9a14f30c59eb0bb6a
Dragon-Git <42856316+Dragon-Git@users.noreply.github.com>
2024-06-13 11:34:33 +0800

    更新 api.sv

--- api.sv
+++ api.sv
@@ -1,6 +1,11 @@
 module top ();
-    import "DPI-C" task body();
+    task wait_unit();
+        #1;
+        $display("inside sv task in %d", $time);
+    endtask
+    export "DPI-C" task wait_unit;
+    import "DPI-C" context task py_func(input string py_seq);
     initial begin
-        body();
+        py_func("run");
     end
 endmodule
