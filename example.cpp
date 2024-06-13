0a6336ca3aa69051f477cd3b804e093f62518abe
Dragon-Git <42856316+Dragon-Git@users.noreply.github.com>
2024-06-13 11:25:22 +0800

    更新 example.cpp

--- example.cpp
+++ example.cpp
@@ -1,25 +1,25 @@
 #include <pybind11/pybind11.h>
 #include <pybind11/embed.h> // everything needed for embedding
 #include "svdpi.h"
+
+extern "C" {
+
 namespace py = pybind11;
 
-extern "C" void body() {
-    py::scoped_interpreter guard{}; // start the interpreter and keep it alive
+void wait_unit();
 
-    py::module_ sys = py::module_::import("sys");
-    py::print(sys.attr("path"));
-    py::module_ run = py::module_::import("run");
-    py::object result = run.attr("add")(1, 2);
-    int n = result.cast<int>();
-    assert(n == 3);
-}
-
-int add(int i, int j) {
-    return i + j;
-}
-
-PYBIND11_MODULE(example, m) {
+PYBIND11_MODULE(svuvm, m) {
     m.doc() = "pybind11 example plugin"; // 可选模块文档字符串
 
-    m.def("add", &add, "A function which adds two numbers");
+    m.def("wait_unit", &wait_unit, "wait unit time");
+}
+
+void body(const char* py_seq) {
+    py::scoped_interpreter guard{}; // start the interpreter and keep it alive
+
+    py::module_ py_seq = py::module_::import(py_seq);
+    py_seq.attr("main")();
+}
+
+
 }
