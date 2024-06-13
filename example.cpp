ad26e2d8dd27af90ca54c450bb67e79f9487dd07
Dragon-Git <42856316+Dragon-Git@users.noreply.github.com>
2024-06-13 11:36:46 +0800

    更新 example.cpp

--- example.cpp
+++ example.cpp
@@ -1,24 +1,24 @@
 #include <pybind11/pybind11.h>
-#include <pybind11/embed.h> // everything needed for embedding
+#include <pybind11/embed.h>
 #include "svdpi.h"
 
 extern "C" {
 
 namespace py = pybind11;
 
-void wait_unit();
+void wait_unit(int n);
 
 PYBIND11_MODULE(svuvm, m) {
-    m.doc() = "pybind11 example plugin"; // 可选模块文档字符串
+    m.doc() = "svuvm api module";
 
     m.def("wait_unit", &wait_unit, "wait unit time");
 }
 
-void body(const char* py_seq) {
+void py_func(const char* py_seq) {
     py::scoped_interpreter guard{}; // start the interpreter and keep it alive
 
-    py::module_ py_seq = py::module_::import(py_seq);
-    py_seq.attr("main")();
+    py::module_ py_seq_mod = py::module_::import(py_seq);
+    py_seq_mod.attr("main")();
 }
 
 
