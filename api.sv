dbe79a1cecb89da9b57c65847c929f89429c221a
Dragon-Git <42856316+Dragon-Git@users.noreply.github.com>
2024-06-14 12:30:43 +0800

    更新 api.sv

--- api.sv
+++ api.sv
@@ -1,11 +1,8 @@
 module top ();
-    task wait_unit(int n);
-        #n;
-        $display("inside sv task in %d", $time);
-    endtask
-    export "DPI-C" task wait_unit;
-    import "DPI-C" context task py_func(input string py_seq);
+    import uvm_pkg::*;
+    import sv_uvm_pkg::*;
+    `include "uvm_macros.svh"
     initial begin
-        py_func("run");
+        call_py_func("run");
     end
 endmodule
