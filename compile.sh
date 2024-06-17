3dba58f6c03a346148745d776f4534665079e55f
Dragon-Git <42856316+Dragon-Git@users.noreply.github.com>
2024-06-17 08:25:47 +0800

    更新 compile.sh

--- compile.sh
+++ compile.sh
@@ -1,2 +1,4 @@
-g++ -O3 -Wall -shared -std=c++17 -fPIC  -undefined dynamic_lookup $(python3 -m pybind11 --includes) src/svuvm.cpp -o svuvm$(python3-config --extension-suffix) $(python3-config --ldflags --embed) -I inc
-verilator --binary +define+NOUVM -top top test/svuvm_pkg.sv test/api.sv svuvm.cpython-312-darwin.so -LDFLAGS "$(python3-config --ldflags --embed)"
+#!/bin/bash
+# g++ -O3 -Wall -shared -std=c++17 -fPIC  -undefined dynamic_lookup $(python3 -m pybind11 --includes) src/svuvm.cpp -o svuvm$(python3-config --extension-suffix) $(python3-config --ldflags --embed) -I inc
+verilator --binary +define+NOUVM -top top test/svuvm_pkg.sv test/api.sv $(python -c "import sysconfig; print(sysconfig.get_path('svuvm'))")/svuvm.cpython-312-darwin.so -LDFLAGS "$(python3-config --ldflags --embed)"
+./obj_dir/Vtop
