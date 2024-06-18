9a65eb3883d48c03ab6e6a7b61f8441040d7f402
Dragon-Git <42856316+Dragon-Git@users.noreply.github.com>
2024-06-18 18:48:32 +0800

    更新 svuvm.cpp

--- src/svuvm.cpp
+++ src/svuvm.cpp
@@ -10,12 +10,17 @@ namespace py = pybind11;
 
 void wait_unit(int n);
 void start_seq(const char* seq_name, const char* sqr_name);
+void write_reg(int address, int data);
+void read_reg(int address, int *data);
 
 PYBIND11_MODULE(svuvm, m) {
     m.doc() = "svuvm api module";
 
     m.def("wait_unit", &wait_unit, "wait unit time");
     m.def("start_seq", &start_seq, "start seq on sqr");
+    m.def("write_reg", &write_reg, "write register");
+    m.def("read_reg", &read_reg, "read data");
+
 }
 
 void py_func(const char* mod_name, const char* func_name, const char* mod_paths) {
