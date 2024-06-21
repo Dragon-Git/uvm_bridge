8546fc76a19a5b5b1ce5c1c892601f068a98c16f
Dragon-Git <1762578117@qq.com>
2024-06-21 22:11:00 +0800

    fixed ci

--- compile.sh
+++ compile.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # g++ -O3 -Wall -shared -std=c++17 -fPIC  -undefined dynamic_lookup $(python3 -m pybind11 --includes) src/svuvm.cpp -o svuvm$(python3-config --extension-suffix) $(python3-config --ldflags --embed) -I inc
-verilator --binary +define+NOUVM -top top $(python -m pysvuvm.config) -LDFLAGS "$(python3-config --ldflags --embed)"  $(PWD)/test/api.sv
+verilator --binary +define+NOUVM -top top $(python -m pysvuvm.config) -LDFLAGS "$(python3-config --ldflags --embed)"  $PWD/test/api.sv
 ./obj_dir/Vtop
