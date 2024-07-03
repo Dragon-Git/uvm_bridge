f7902c5d142443a3e242d014813cb1164372fce1
Dragon-Git <42856316+Dragon-Git@users.noreply.github.com>
2024-07-03 11:03:52 +0800

    添加uvm

--- src/pysvuvm/svuvm.cpp
+++ src/pysvuvm/svuvm.cpp
@@ -8,17 +8,7 @@ extern "C" {
 
 namespace py = pybind11;
 #if defined(VCS) || defined(VCSMX) || defined(XCELIUM) || defined(NCSC)
-#include "uvm_dpi.h"
-#else
-void m_uvm_report_dpi(int severity,
-                      char* id,
-                      char* message,
-                      int verbosity,
-                      char* file,
-                      int linenum){
-                        return;
-                      };
-    
+#include "uvm_dpi.h"    
 #endif
 void wait_unit(int n);
 void start_seq(const char* seq_name, const char* sqr_name);
@@ -31,10 +21,78 @@ int read_reg_wrap(int address) {
     return data;
 }
 
+#if defined(VCS) || defined(VCSMX) || defined(XCELIUM) || defined(NCSC)
+PYBIND11_MODULE(uvmdpi, m) {
+    m.attr("UVM_INFO") = M_UVM_INFO;
+    m.attr("UVM_WARNING") = M_UVM_WARNING;
+    m.attr("UVM_ERROR") = M_UVM_ERROR;
+    m.attr("UVM_FATAL") = M_UVM_FATAL;
+
+    m.attr("UVM_NONE") = M_UVM_NONE;
+    m.attr("UVM_LOW") = M_UVM_LOW;
+    m.attr("UVM_MEDIUM") = M_UVM_MEDIUM;
+    m.attr("UVM_HIGH") = M_UVM_HIGH;
+    m.attr("UVM_FULL") = M_UVM_FULL;
+    m.attr("UVM_DEBUG") = M_UVM_DEBUG;
+
+    // Binding functions
+    m.def("uvm_report", &m_uvm_report_dpi, "report function", py::arg("severity"), py::arg("id"), py::arg("message"), py::arg("verbosity"), py::arg("file"), py::arg("linenum"));
+
+    m.def("int_str_max", &int_str_max, "Find the maximum of integers represented as strings.");
+
+    m.def("uvm_re_match", &uvm_re_match, "Match a regular expression against a string.",
+          py::arg("re"), py::arg("str"));
+
+    m.def("uvm_glob_to_re", &uvm_glob_to_re, "Convert a glob pattern into a regular expression.",
+          py::return_value_policy::reference);
+
+    m.def("uvm_hdl_check_path", &uvm_hdl_check_path, "Check if a path exists in the HDL model.",
+          py::arg("path"));
+
+    m.def("uvm_hdl_read", &uvm_hdl_read, "Read data from a path in the HDL model.",
+          py::arg("path"), py::arg("value"));
+
+    m.def("uvm_hdl_deposit", &uvm_hdl_deposit, "Deposit data at a path in the HDL model.",
+          py::arg("path"), py::arg("value"));
+
+    m.def("uvm_hdl_force", &uvm_hdl_force, "Force a value at a path in the HDL model.",
+          py::arg("path"), py::arg("value"));
+
+    m.def("uvm_hdl_release_and_read", &uvm_hdl_release_and_read, "Release and read data from a path in the HDL model.",
+          py::arg("path"), py::arg("value"));
+
+    m.def("uvm_hdl_release", &uvm_hdl_release, "Release a path in the HDL model.",
+          py::arg("path"));
+
+    m.def("push_data", &push_data, "Push data to a specified level.",
+          py::arg("lvl"), py::arg("entry"), py::arg("cmd"));
+
+    m.def("walk_level", &walk_level, "Walk through a hierarchy at a given level.",
+          py::arg("lvl"), py::arg("argc"), py::arg("argv"), py::arg("cmd"));
+
+    m.def("uvm_dpi_get_next_arg_c", &uvm_dpi_get_next_arg_c, "Get the next argument from the command line.",
+          py::arg("init"));
+
+    m.def("uvm_dpi_get_tool_name_c", &uvm_dpi_get_tool_name_c, "Get the name of the current tool.",
+          py::return_value_policy::reference);
+
+    m.def("uvm_dpi_get_tool_version_c", &uvm_dpi_get_tool_version_c, "Get the version of the current tool.",
+          py::return_value_policy::reference);
+
+    m.def("uvm_dpi_regcomp", &uvm_dpi_regcomp, "Compile a regular expression.",
+          py::arg("pattern"), py::return_value_policy::take_ownership);
+
+    m.def("uvm_dpi_regexec", &uvm_dpi_regexec, "Execute a compiled regular expression against a string.",
+          py::arg("re"), py::arg("str"));
+
+    m.def("uvm_dpi_regfree", &uvm_dpi_regfree, "Free a compiled regular expression.",
+          py::arg("re"));
+}
+#endif
+
 PYBIND11_MODULE(svuvm, m) {
     m.doc() = "svuvm api module";
 
-    m.def("uvm_report", &m_uvm_report_dpi, "uvm report by dpi");
     m.def("wait_unit", &wait_unit, "wait unit time");
     m.def("start_seq", &start_seq, "start seq on sqr");
     m.def("write_reg", &write_reg, "write register");
