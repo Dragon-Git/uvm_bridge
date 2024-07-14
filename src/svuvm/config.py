94cd0a6a08a79062a8384ef26773bca6dc2ff366
Dragon-Git <1762578117@qq.com>
2024-07-14 21:47:33 +0800

    fixed path

--- src/svuvm/config.py
+++ src/svuvm/config.py
@@ -4,7 +4,7 @@ from pathlib import Path
 getvar = sysconfig.get_config_var
 so_name = "svuvm" + getvar("EXT_SUFFIX")
 
-so_file = Path(__file__).parent.parent / so_name
+so_file = Path(__file__).parent / so_name
 sv_file = Path(__file__).parent / "python_bridge_pkg.sv"
 
 print(f'{sv_file} {so_file}')
