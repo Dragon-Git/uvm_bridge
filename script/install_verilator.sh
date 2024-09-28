84d0bf3d312266d79a781291bf5f09e6b8254268
Dragon-Git <1762578117@qq.com>
2024-09-28 21:32:33 +0800

    [ci] cache verilator

---
+++ script/install_verilator.sh
@@ -0,0 +1,26 @@
+#! /bin/bash
+
+# VERILATOR_ROOT="$PWD/verilator"
+
+# Clone repo only if it doesn't exist
+if [ ! -d /home/runner/work/verilator ]; then
+    cd /home/runner/work/
+    git clone https://github.com/verilator/verilator.git
+fi
+
+cd /home/runner/work/verilator
+
+# Returns non-zero if no .o files exist
+find -name '*.o' | grep -q .
+
+# If .o files don't exist, do autoconf and do make
+if [ $? -ne 0 ]; then
+    git checkout v5.028
+    autoconf
+    ./configure
+    make -j `nproc`
+else
+    echo "Verilator already built, skipping compile/install steps"
+fi
+make install
+
