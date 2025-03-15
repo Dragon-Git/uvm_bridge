a8765c566584a23fafec7e88ddbe4d8ea09ff458
Dragon-Git <1762578117@qq.com>
2025-03-15 14:58:48 +0800

    custom patch for upstream uvm

--- compile.sh
+++ compile.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 # g++ -O3 -Wall -shared -std=c++17 -fPIC  -undefined dynamic_lookup $(python3 -m pybind11 --includes) src/svuvm.cpp -o svuvm$(python3-config --extension-suffix) $(python3-config --ldflags --embed) -I inc
-verilator -j $(nproc) --vpi --binary +define+UVM_NO_DPI -top top -Mdir build -MAKEFLAGS VM_PARALLEL_BUILDS=O --output-groups $(($(nproc)*4)) \
+verilator -j $(nproc) --vpi --binary -top top -Mdir build -MAKEFLAGS VM_PARALLEL_BUILDS=O --output-groups $(($(nproc)*4)) \
 +incdir+$PWD/uvm-verilator/src $PWD/uvm-verilator/src/uvm_pkg.sv \
 $(python -m svuvm.config) -LDFLAGS "$(python3-config --ldflags --embed)" \
 $PWD/test/api.sv \
 -Wno-lint -Wno-style -Wno-SYMRSVDWORD -Wno-IGNOREDRETURN -Wno-CONSTRAINTIGN -Wno-ZERODLY \
-
-# $PWD/uvm-verilator/src/dpi/uvm_dpi.cc \
-# -CFLAGS "-I$PWD/uvm-verilator/src/dpi -I$PWD/inc" \
-# -DUVM_CMDLINE_NO_DPI -DUVM_REGEX_NO_DPI -DUVM_HDL_NO_DPI \
+$PWD/uvm-verilator/src/dpi/uvm_dpi.cc \
+-CFLAGS "-I$PWD/uvm-verilator/src/dpi -I$PWD/inc" \
 
 ./build/Vtop
