785b73a0e89aab01debd85ff9d7fa216cc5077e3
Dragon-Git <1762578117@qq.com>
2025-03-16 14:02:40 +0800

    ci 编译加速

--- compile.sh
+++ compile.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # g++ -O3 -Wall -shared -std=c++17 -fPIC  -undefined dynamic_lookup $(python3 -m pybind11 --includes) src/svuvm.cpp -o svuvm$(python3-config --extension-suffix) $(python3-config --ldflags --embed) -I inc
-verilator -j $(nproc) --vpi --binary --timing -top top -Mdir build -MAKEFLAGS VM_PARALLEL_BUILDS=O --output-groups $(($(nproc)*4)) \
+verilator -j $(nproc) --vpi --binary --timing -top top -Mdir build --output-groups $(($(nproc)*2)) \
 +incdir+$PWD/uvm-verilator/src $PWD/uvm-verilator/src/uvm_pkg.sv \
 $(python -m svuvm.config) \
 $PWD/test/api.sv \
