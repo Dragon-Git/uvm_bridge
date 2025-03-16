eb7675f50d3287abe5b1d03ec396012418691cdd
Dragon-Git <1762578117@qq.com>
2025-03-16 13:43:59 +0800

    remove python flags

--- compile.sh
+++ compile.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 # g++ -O3 -Wall -shared -std=c++17 -fPIC  -undefined dynamic_lookup $(python3 -m pybind11 --includes) src/svuvm.cpp -o svuvm$(python3-config --extension-suffix) $(python3-config --ldflags --embed) -I inc
-verilator -j $(nproc) --vpi --binary -top top -Mdir build -MAKEFLAGS VM_PARALLEL_BUILDS=O --output-groups $(($(nproc)*4)) \
+verilator -j $(nproc) --vpi --binary --timing -top top -Mdir build -MAKEFLAGS VM_PARALLEL_BUILDS=O --output-groups $(($(nproc)*4)) \
 +incdir+$PWD/uvm-verilator/src $PWD/uvm-verilator/src/uvm_pkg.sv \
-$(python -m svuvm.config) -LDFLAGS "$(python3-config --ldflags --embed)" \
+$(python -m svuvm.config) \
 $PWD/test/api.sv \
 -Wno-lint -Wno-style -Wno-SYMRSVDWORD -Wno-IGNOREDRETURN -Wno-CONSTRAINTIGN -Wno-ZERODLY \
 $PWD/uvm-verilator/src/dpi/uvm_dpi.cc \
 -CFLAGS "-I$PWD/uvm-verilator/src/dpi -I$PWD/inc" \
 
-./build/Vtop
+./build/Vtop +UVM_NO_RELNOTES
