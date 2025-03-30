5843adb7ca85f80ed7c6f30d17d34be41c2c3b98
Dragon-Git <1762578117@qq.com>
2025-03-30 17:07:59 +0800

    add UVM_OBJECTION_TRACE

--- compile.sh
+++ compile.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # g++ -O3 -Wall -shared -std=c++17 -fPIC  -undefined dynamic_lookup $(python3 -m pybind11 --includes) src/svuvm.cpp -o svuvm$(python3-config --extension-suffix) $(python3-config --ldflags --embed) -I inc
-verilator -j $(nproc) --vpi --binary --timing -top top -Mdir build --output-groups $(($(nproc)*2)) \
+verilator -j $(nproc) --vpi --binary -top top -Mdir build --output-groups $(($(nproc)*2)) \
 +incdir+$PWD/uvm-verilator/src $PWD/uvm-verilator/src/uvm_pkg.sv \
 $(python -m svuvm.config) \
 $PWD/test/api.sv \
@@ -8,4 +8,4 @@ $PWD/test/api.sv \
 $PWD/uvm-verilator/src/dpi/uvm_dpi.cc \
 -CFLAGS "-I$PWD/uvm-verilator/src/dpi -I$PWD/inc" \
 
-./build/Vtop +UVM_NO_RELNOTES
+./build/Vtop +UVM_NO_RELNOTES +UVM_OBJECTION_TRACE
